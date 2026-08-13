ea2a40a6f85d5389aeabf4b4f2c6279a1977e95e
Vinicius Benevides <massaki1999@gmail.com>
2026-08-13 18:37:22 -0300

    feat(controlplane): run HTTP control plane

--- backend/cmd/controlplane/main.go
+++ backend/cmd/controlplane/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/VBenevides/Glyphflow/backend/internal/api"
 	"github.com/VBenevides/Glyphflow/backend/internal/config"
 )
 
@@ -12,5 +17,15 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	server := &http.Server{Addr: ":8080", Handler: api.Server{Auth: func(*http.Request) (api.Claims, bool) {
+		return api.Claims{Subject: "local", Roles: map[string]bool{"task.read": true}}, true
+	}}.Handler()}
+	go func() { <-ctx.Done(); _ = server.Shutdown(context.Background()) }()
 	fmt.Println("Glyphflow control plane")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
