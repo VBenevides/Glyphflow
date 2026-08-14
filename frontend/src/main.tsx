2514c561d549fd4fc99a6b24162f41ace45f7b7e
Vinicius Benevides <massaki1999@gmail.com>
2026-08-14 16:38:13 -0300

    feat(assets): Include favicon and multiple versions of icon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import glyphflowIcon from '../../assets/glyphflow.png'
+
+const favicon = document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.href = glyphflowIcon
+document.head.append(favicon)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
